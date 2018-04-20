8972437ecc1883ac5110c09a48d57779db7e273d
Ana María <anamariasosam@gmail.com>
2018-04-20 06:46:31 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,27 @@
+passwd
+date
+who
+echo "Hola"
+man date
+df
+du
+ls
+mkdir dir1
+rmdir dir1
+pwd
+cd dir1
+cd ../..
+cd 
+pico proci
+clear
+sleep 4 (n segundos)
+ls -l 
+$HOME # Mi ruta
+
+# Manipulacion de archivos
+cp
+cp prac1 prac2 # copio el archivo o lo creo, sobreescribiendo
+mv
+mv prac prac1 # cambio el nombre o lo muevo
+rm # borra el archivo
+rm * -r # borra TOOODOOO
