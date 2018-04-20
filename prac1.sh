be5fcaa5edb7ba4bd50af0205c9a9bab0a1b4fe6
Ana María <anamariasosam@gmail.com>
2018-04-20 06:49:58 -0500

    Create prac1.sh

---
+++ prac1.sh
@@ -0,0 +1,21 @@
+echo "Ana Maria Sosa"
+echo "Crear carpeta"
+mdkir UDEM
+sleep 5
+clear
+echo "Ruta en donde estoy"
+pwd
+sleep 5
+clear
+echo "Los que están conectados son"
+who
+sleep 5 
+clear
+echo "Espacio libre en el disco"
+df
+sleep 5
+clear
+echo "Espacio utilizado"
+du
+sleep 5
+clear
