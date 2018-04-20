5ac137d475b4369b88e07557e357823c4ede3f23
Ana María <anamariasosam@gmail.com>
2018-04-20 06:53:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,6 +18,8 @@ sleep 4 (n segundos)
 ls -l 
 $HOME # Mi ruta
 
+sh prac1 # Ejecuta el programa prac 1
+
 # Manipulacion de archivos
 cp
 cp prac1 prac2 # copio el archivo o lo creo, sobreescribiendo
@@ -25,3 +27,5 @@ mv
 mv prac prac1 # cambio el nombre o lo muevo
 rm # borra el archivo
 rm * -r # borra TOOODOOO
+cat prac1 # Imprime el archivo
+head -n archivo # Muesta las primeras n lineas del archivo prac1
